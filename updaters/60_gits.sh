1ae3e73e1aaef593cb9321fc2731375394a63129
chorn <chorn@chorn.com>
2017-11-16 18:52:25 -0500

    Add a gits updater
    If you specify a list of directories to search in your `~/.dtu` this
    updater will look for git repositories and `git pull` on any that are
    clean.

---
+++ updaters/60_gits.sh
@@ -0,0 +1,18 @@
+#-----------------------------------------------------------------------------
+updater() {
+  command -v git >& /dev/null || return
+  [[ -z ${tools[*]} ]] && return
+
+  for tool in ${tools[@]} ; do
+    tool=$(eval "echo ${tool}")
+
+    while read -r _git_dir ; do
+      local _dir=$(dirname "$_git_dir")
+      cd "$_dir"
+      git diff --cached --exit-code >&/dev/null || continue
+      git diff --exit-code >&/dev/null || continue
+      git pull --quiet || echo "Problem with git in $_git_dir"
+    done < <(find "$tool" -maxdepth 3 -mindepth 1 -type d -name .git)
+  done
+}
+#-----------------------------------------------------------------------------
