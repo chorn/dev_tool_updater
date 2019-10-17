e2dcbcba11924c74929e35cce20bc932b6a9d510
chorn <chorn@chorn.com>
2019-10-17 09:53:14 -0400

    Change pip list arg

--- updaters/pip.sh
+++ updaters/pip.sh
@@ -1,6 +1,6 @@
 #-----------------------------------------------------------------------------
 updater() {
-  basic_list_install_and_upgrade "pip2" "pip2 install" "pip2 install --upgrade" "pip2 list --format=legacy | cut -f 1 -d ' '"
-  basic_list_install_and_upgrade "pip3" "pip3 install" "pip3 install --upgrade" "pip3 list --format=legacy | cut -f 1 -d ' '"
+  basic_list_install_and_upgrade "pip2" "pip2 install" "pip2 install --upgrade" "pip2 list --format columns | cut -f 1 -d ' '"
+  basic_list_install_and_upgrade "pip3" "pip3 install" "pip3 install --upgrade" "pip3 list --format columns | cut -f 1 -d ' '"
 }
 #-----------------------------------------------------------------------------
